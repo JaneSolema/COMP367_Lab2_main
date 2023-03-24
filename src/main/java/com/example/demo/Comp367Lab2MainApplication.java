a817efaf57370f0404139648e7ff618863a18f8e
JaneSolema <105816842+JaneSolema@users.noreply.github.com>
2023-03-24 19:56:41 -0400

    First Commit

---
+++ src/main/java/com/example/demo/Comp367Lab2MainApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comp367Lab2MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Comp367Lab2MainApplication.class, args);
+		System.out.println("Springboot starting. . .");
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>COMP367 - LAB ASSIGNMENT 2</title>
+</head>
+<body>
+<h1>${message}, Jane, Welcome to COMP367</h1>
+</body>
+</html>
